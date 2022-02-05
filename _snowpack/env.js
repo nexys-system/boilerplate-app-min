01d9b9d86d726bca96b0cb20f7d2893fd6f90a24
johnb8005 <johnb8005@users.noreply.github.com>
2022-02-05 05:20:38 +0000

    deploy: da1bba578e9bf92073cb89003eb4a9f842e6d929

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,6 +1,6 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "98f1270b6fe600e0bce3ad5247f98490c4715dff";
-export const SNOWPACK_PUBLIC_VERSION = "v0.0.2";
-export const SNOWPACK_PUBLIC_URL = "/boilerplate-app-min";
+export const SNOWPACK_PUBLIC_GIT_SHA = "da1bba578e9bf92073cb89003eb4a9f842e6d929";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.3";
+export const SNOWPACK_PUBLIC_URL = "";
 export const MODE = "development";
 export const NODE_ENV = "development";
 export const SSR = false;
